9de4d1900804cb9d29a12b2b0f8a62e584295fc0
Gran Fuego <74319971+granfuego@users.noreply.github.com>
2024-03-02 22:43:10 +0200

    Added fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'east_22' -- discord: east_22 | https://github.com/granfuego (check github for latest versions)
+description 'Simple job center for ESX'
+version '1.0'
+
+shared_script '@es_extended/imports.lua'
+
+client_script 'client.lua'
+server_script 'server.lua'
